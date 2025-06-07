f48256ae8193ecc6eacfc365fd9f99f79c716e10
Andrij Kostecki <kostecki.andrij@gmail.com>
2025-06-07 14:42:54 +0200

    Add dataModel

---
+++ src/main/java/org/example/DataModel/DataModel.java
@@ -0,0 +1,16 @@
+package src.main.java.DataModel;
+
+import java.util.ArrayList;
+
+public class DataModel {
+    private String Name;
+    private ArrayList<Project> ProjectList = new ArrayList<>();
+    public DataModel(String Name, Project project) {
+        this.Name = Name;
+
+    }
+    public ArrayList<Project> getTaskList() {
+        return ProjectList;
+    }
+
+}
---
+++ src/main/java/org/example/DataModel/Project.java
@@ -0,0 +1,20 @@
+package src.main.java.DataModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Project {
+    private String Name;
+    private ArrayList<Task> taskList = new ArrayList<>();
+
+    public Project(String name, List<Task> taskList) {
+        this.Name = name;
+    }
+    public String getName() {
+        return Name;
+    }
+
+    public ArrayList<Task> getTaskList() {
+        return taskList;
+    }
+}
