d62ad80de3955826110db4f9cbfc5f9cb181c689
Andrij Kostecki <kostecki.andrij@gmail.com>
2025-06-07 14:08:03 +0200

    DataModel Classes implementation

---
+++ src/main/java/DataModel/Employee.java
@@ -0,0 +1,24 @@
+package src.main.java.DataModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+    private String SurnameName;
+    private List<Task> taskList = new ArrayList<>();
+
+    public Employee(String SurnameName) {
+        this.SurnameName = SurnameName;
+    }
+
+    public String getSurnameName() {
+        return SurnameName;
+    }
+
+
+    public List<Task> getTaskList() {
+        return taskList;
+    }
+
+
+}
